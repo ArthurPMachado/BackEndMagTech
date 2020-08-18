4ac1143559808b9dc7735581a068f6cfec68d395
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-18 11:20:41 -0300

    Feat: add post and usuario comum repositories

---
+++ magtech/src/main/java/br/com/fiap/magtech/repository/UsuarioComumRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.magtech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.magtech.entity.UsuarioComum;
+
+public interface UsuarioComumRepository extends JpaRepository<UsuarioComum, Integer>{
+
+}
